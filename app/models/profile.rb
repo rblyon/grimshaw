04664288cde9d718381a9d7b7e075ae4baed62f3
Richard B Lyon <rblyon@gmail.com>
2012-11-23 18:41:20 +0000

    grimshawed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,15 +2,28 @@
 #
 # Table name: profiles
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  fullname   :string(255)
-#  bitrate    :integer
-#  container  :string(255)
-#  codec      :string(255)
-#  level      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  bitrate            :integer
+#  codec              :string(255)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  map_pid            :string(255)
+#  map_revision       :integer
+#  description        :text
+#  format             :string(255)
+#  framerate          :string(255)
+#  height             :string(255)
+#  asset_type         :string(255)
+#  mime_type          :string(255)
+#  video_bitrate      :integer
+#  video_mode         :string(255)
+#  width              :string(255)
+#  audio_bitrate      :integer
+#  audio_codec        :string(255)
+#  audio_mode         :string(255)
+#  map_link           :string(255)
+#  video_bitrate_mode :string(255)
 #
 
 class Profile < ActiveRecord::Base
