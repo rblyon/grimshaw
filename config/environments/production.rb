999fb424a15f124f2565fa4db1b95cc9cb89a5f5
Richard B Lyon <rblyon@gmail.com>
2012-11-26 10:04:19 +0000

    basic authentiation on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ Grim1::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Production") do |u, p|
+      [u, p] == ['podprofiles', 'n0rman5']
+    end
+  
+  
 end
